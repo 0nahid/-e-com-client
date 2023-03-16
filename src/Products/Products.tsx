fc3b0860f1591ea281cb2fd0869b69c75c606f1e
0nahid <nahidhassanbulbul@gmail.com>
2023-03-16 22:52:38 +0600

    added => pagination:limit,page

--- src/Products/Products.tsx
+++ src/Products/Products.tsx
@@ -1,17 +1,34 @@
+import Pagination from "@/Shared/Pagination";
 import { useQuery } from "@tanstack/react-query";
+import { useRouter } from 'next/router';
+import { useEffect, useState } from "react";
 
 export default function Products() {
-    const products = GetProducts();
+    const [limit, setLimit] = useState<number>(10);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const router = useRouter();
+    const { query } = router;
+    console.log(query);
+
+    const products = GetProducts(query);
+    useEffect(() => {
+        router.query.limit = limit.toString();
+        router.query.page = currentPage.toString();
+        router.push(router)
+    }, [limit, currentPage])
+
+
     if (products?.data?.length === 0) return <h1>No products</h1>
-    console.log(products);
 
     return (
         <>
             <div className="grid sm:grid-cols-1 md:grid-cols-4 lg:grid-cols-6 gap-10 justify-items-center">
                 {products?.data?.map((product: any) => (
                     <div className="card card-compact card-bordered w-75 bg-base-100 shadow-xl" key={product._id}>
-                        <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
                         <div className="card-body">
+                            <figure>
+                                <img src="" alt="" />
+                            </figure>
                             <h5 className="card-title">
                                 {product?.name?.slice(0, 25) + (product?.name?.length > 25 ? "..." : "")}
                             </h5>
@@ -25,15 +42,25 @@ export default function Products() {
                     </div>
                 ))}
             </div>
+
+            {/* pagination */}
+            <div className="flex justify-center">
+                <Pagination total={products?.total} limit={limit} currentPage={currentPage} setCurrentPage={setCurrentPage}
+                    setLimit={setLimit}
+                />
+            </div>
         </>
     )
 }
 
 
-function GetProducts() {
+function GetProducts(query: any) {
     const { data } = useQuery({
-        queryKey: ["products"],
-        queryFn: () => fetch(`http://localhost:5000/api/v1/products`).then(res => res.json())
+        queryKey: ["products", query?.page, query?.limit, query?.sort, query?.order, query?.search, query?.category, query?.brand, query?.rating, query?.inStock, query?.fastDelivery],
+        queryFn: () => fetch(`http://localhost:5000/api/v1/products?limit=${query.limit}&page=${query.page}`).then(res => res.json())
     })
-    return data
+    return {
+        data: data?.data,
+        total: data?.count,
+    }
 }
---
+++ src/Shared/Pagination.tsx
@@ -0,0 +1,36 @@
+
+interface PaginationProps {
+    total?: number;
+    limit?: number;
+    currentPage?: number;
+    setCurrentPage?: (page: number) => void;
+    setLimit?: (limit: number) => void;
+}
+
+export default function Pagination({ total, limit, currentPage, setCurrentPage, setLimit }: PaginationProps) {
+    if (!total || !limit || !currentPage || !setCurrentPage || !setLimit) return null;
+    const totalPages = Math.ceil(total / limit);
+    const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+    console.log(currentPage);
+    return (
+        <div className="btn-group mt-2">
+            {pages.map((page) => (
+                <button
+                    key={page}
+                    className={`btn ${currentPage === page ? "btn-primary" : ""}`}
+                    onClick={() => setCurrentPage && setCurrentPage(page)}
+                >{page}</button>
+            ))}
+
+            <select
+                className="btn btn-ghost" onChange={(e) => setLimit && setLimit(parseInt(e.target.value))
+                }>
+               {
+                     [10,15,20].map((limit) => (
+                        <option key={limit} value={limit}>{limit}</option>
+                    ))
+               }
+            </select>
+        </div>
+    );
+}
